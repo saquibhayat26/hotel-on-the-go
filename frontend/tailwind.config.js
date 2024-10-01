0d677cf2ce0a0cba08c9f371e35458f427ab6396
saquib.md <saquib.md@techurate.com>
2024-10-01 15:57:38 +0530

    tailwind integrated

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // specify file types to apply tailwind css
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
